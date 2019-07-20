0c991d6a80b2ca628094dd87ae104f6417c647d1
Brandon Brown <Brandon@encoremp.com>
2019-07-20 12:03:37 -0500

    add search handler

---
+++ src/SearchPatientHandler.ts
@@ -0,0 +1,23 @@
+import { patients } from "../src/mockData/patients";
+
+interface IResponseObject {
+    statusCode: number;
+    body: Object;
+    headers: Object;
+}
+
+export function searchPatientHandler(event: any, _context: any, callback: (err: any, res: any) => any) {
+  const res = patients.filter((patientRecord) => patientRecord.patient.id == event.body.patient_key);
+  if(res.length > 0) {
+      callback(null, createResponseObject(200, res[0]));
+  }
+  callback(null, createResponseObject(404, {"message": "error finding patient"}));
+}
+
+function createResponseObject(code: number, body: Object): IResponseObject {
+    return {
+        statusCode: code,
+        body,
+        headers: { 'Access-Control-Allow-Origin': '*' },
+    };
+}
