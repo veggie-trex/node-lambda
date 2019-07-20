02551391378a35eab899ca6f1116b2751f554113
Brandon Brown <Brandon@encoremp.com>
2019-07-20 18:33:40 -0500

    deploy

--- src/TestBCHandler.ts
+++ src/TestBCHandler.ts
@@ -9,7 +9,12 @@ interface IResponseObject {
   export async function testBCHandler(event: any, _context: any, callback: (err: any, res: any) => any) {
     let response = null;
     try {
-      response = await axios.get('http://HH-Nodes-NLB-81c64a05c47dd62f.elb.us-east-2.amazonaws.com');
+      response = await axios.post('http://HH-Nodes-NLB-81c64a05c47dd62f.elb.us-east-2.amazonaws.com:8545', 
+        {
+          "jsonrpc":"2.0",
+          "method":"net_peerCount",
+          "id":64
+     });
       callback(null, createResponseObject(200, response.data));
     } catch(e) {
       callback(null, createResponseObject(200, e));
