f8434c56db031698ce5671b1b8d1aab9d8215234
Brandon Brown <Brandon@encoremp.com>
2019-07-20 14:20:26 -0500

    testing

--- src/SearchPatientHandler.ts
+++ src/SearchPatientHandler.ts
@@ -1,4 +1,23 @@
-import patients from "../src/mockData/patients.json";
+const patients = [{
+    "patient": {
+        "id": "87ABAB1D-4262-431B-B569A0FCB4776426",
+        "name": "Testing Patient",
+        "dob": "04/11/1992",
+        "ssn": "4321",
+        "address": "123 Patient St.",
+        "phone": "773-555-5555"
+    }
+}, 
+{
+    "patient": {
+        "id": "BA0607BD-4F37-474B-B9B6BB9FBFF3AD3D",
+        "name": "Testing Patient 1",
+        "dob": "06/22/1975",
+        "ssn": "2255",
+        "address": "4563 North Fake St.",
+        "phone": "817-555-5555"
+    }
+}];
 
 interface IResponseObject {
     statusCode: number;
