29d730aad03bcfd5c9d2ebeeae61b2d5b6cf0064
Juan Pablo Garcia <garjuanpablo@gmail.com>
2019-07-20 14:04:05 -0500

    Fix test

--- __test__/GetRecordByIdHandler.test.ts
+++ __test__/GetRecordByIdHandler.test.ts
@@ -1,4 +1,4 @@
-import { getRecordByIdHandler } from '../src/GetPatientRecordByIdHandler';
+import { getPatientRecordByIdHandler } from '../src/GetPatientRecordByIdHandler';
 describe('Test GetRecordByIdHandler', () => {
     test('01: getRecordByIdHandler', () => {
         const event = {
@@ -7,7 +7,7 @@ describe('Test GetRecordByIdHandler', () => {
                 recordId: '456'
             }
         }
-        getRecordByIdHandler(event, {}, (arg1, arg2) => {
+        getPatientRecordByIdHandler(event, {}, (arg1, arg2) => {
             console.log('arg1', arg1);
             console.log('arg2', arg2);
         });
