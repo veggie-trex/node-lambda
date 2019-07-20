081fcf0c3f686ea5c181aa7825aa61c8e2e38de2
Juan Pablo Garcia <garjuanpablo@gmail.com>
2019-07-20 11:52:49 -0500

    Add get and post operations

---
+++ src/patients/sql.ts
@@ -0,0 +1,13 @@
+import { sqlTables } from "../dbTools/mySql/tables";
+import { createElementSql } from "../dbTools/mySql/dbTools";
+
+export function getPatientById(patientId: string) {
+    const query = `SELECT * 
+    FROM ${sqlTables.patients}
+    WHERE id = ${patientId}`;
+    return query;
+}
+
+export function createPatient (patient: {name: string}) {
+    return createElementSql(sqlTables.patients, patient);
+  }
