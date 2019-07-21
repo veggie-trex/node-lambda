59d458eb7b46d4ab48aec40ef7e4d890287f316c
Brandon Brown <Brandon@encoremp.com>
2019-07-21 04:00:38 -0500

    Clean dependencies

--- src/dbTools/mySql/dbVariables.ts
+++
@@ -1,6 +0,0 @@
-export const dbVariables = {
-    DB_HOST: process.env.DB_HOST,
-    DB_USER: process.env.DB_USER,
-    DB_PASSWORD: process.env.DB_PASSWORD,
-    DB_NAME: process.env.DB_NAME
-}
--- src/dbTools/mySql/tables.ts
+++
@@ -1,3 +0,0 @@
-export const sqlTables = {
-  patients: 'Patient'
-};
