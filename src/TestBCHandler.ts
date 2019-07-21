234a73e2e2fb65ee07e5a80fcaabbd7d1866f0e9
Brandon Brown <Brandon@encoremp.com>
2019-07-20 20:55:01 -0500

    push it

--- src/TestBCHandler.ts
+++ src/TestBCHandler.ts
@@ -9,30 +9,40 @@ interface IResponseObject {
 
   export async function testBCHandler(event: any, _context: any, callback: (err: any, res: any) => any) {
     process.chdir('/tmp');
-    
-    const nlbURL = process.env.NLB_URL;
-    console.log('HIT')
-    console.log(nlbURL);
-    try {
-      const web3 = new Web3(new Web3.providers.HttpProvider(nlbURL));
-      console.log(web3.eth.blockNumber);
-      console.log(web3.net.peerCount);
-    } catch(e) {
-      console.log(e);
-    }
 
-    callback(null, createResponseObject(200, 'Done'));
-    // let response = null;
-    // try {
-    //   response = await axios.post('http://HH-Nodes-NLB-81c64a05c47dd62f.elb.us-east-2.amazonaws.com:8545', 
-    //     {
-    //       "jsonrpc":"2.0",
-    //       "method":"net_peerCount",
-    //       "id":64
-    //  });
-    // } catch(e) {
-    //   callback(null, createResponseObject(200, e));
-    // }
+    // 1. Get Required Input Params & Validate
+    console.log('enter');
+    const params = JSON.parse(event.body);
+    if(!params) {
+      callback(null, createResponseObject(400, 'Invalid request'));
+      return;
+    }
+    const {address} = params;
+    console.log(params);
+    // VALIDATE ADDRESS IS NOT EMPTY
+    if(!address) {
+        callback(null, createResponseObject(400, 'Invalid request'));
+        return;
+    }
+    console.log('valid');
+    // 2. Get Environment Variables / Constants
+    const nodeUrl = process.env.NLB_URL;
+    const contractAddress = process.env.CONTRACT_ADDRESS;
+    const contractABI = getContractABI();
+    console.log(contractABI);
+    // 3. Get Access to Smart Contract
+    const web3 = new Web3(new Web3.providers.HttpProvider(nodeUrl));
+    const healthcareContract = web3.eth.contract(contractABI);
+    const healthcareInstance = healthcareContract.at(contractAddress);
+    console.log('have an instance');
+    const records = healthcareInstance.getRecordsForUser(address).split(';');
+    console.log(records);
+    callback(null, createResponseObject(200, JSON.stringify(records)));
+  }
+
+  function getContractABI() {
+    let ABI = [{"constant":false,"inputs":[{"name":"who","type":"address"},{"name":"record","type":"string"}],"name":"storeRecordForUser","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"who","type":"address"}],"name":"getRecordsForUser","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"}]
+    return ABI
   }
 
   function createResponseObject(code: number, body: Object): IResponseObject {
