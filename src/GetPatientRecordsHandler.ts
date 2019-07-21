28c511a518c99a026494de1e6d92ed95313ea274
Brandon Brown <Brandon@encoremp.com>
2019-07-21 09:48:47 -0500

    push it

--- src/GetPatientRecordsHandler.ts
+++ src/GetPatientRecordsHandler.ts
@@ -11,6 +11,30 @@ const res = [{
 	immunizationType: "IM-MS-0001",
 	immunizationDate: 1563636169,
 	nextImmunizationDate: 1593820800
+},{
+  doctorId: "doctorId-456",
+  patientId: "patientId-321",
+  immunizationType: "IM-MS-0261",
+  immunizationDate: 1563636169,
+  nextImmunizationDate: 1593820800
+},{
+  doctorId: "doctorId-553",
+  patientId: "patientId-245",
+  immunizationType: "IM-MS-0191",
+  immunizationDate: 1563636169,
+  nextImmunizationDate: 1593820800
+},{
+  doctorId: "doctorId-553",
+  patientId: "patientId-2343",
+  immunizationType: "IM-MS-02341",
+  immunizationDate: 1563636169,
+  nextImmunizationDate: 1593820800
+},{
+  doctorId: "doctorId-45453",
+  patientId: "patientId-553",
+  immunizationType: "IM-MS-23424",
+  immunizationDate: 1563636169,
+  nextImmunizationDate: 1593820800
 }]
 callback(null, createResponseObject(200, JSON.stringify(res)));
 }
